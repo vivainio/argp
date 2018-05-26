9ae2b7737119736322d03c5ae5ccd26c77c83c1d
Ville M. Vainio <ville.vainio@basware.com>
2018-05-26 13:50:14 +0300

    add files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='argp',
+      version='0.1.0',
+      description='Thin wrapper for argparse',
+      author='Ville M. Vainio',
+      author_email='vivainio@gmail.com',
+      url='https://github.com/vivainio/argp',
+      packages=['argp'],
+     )
